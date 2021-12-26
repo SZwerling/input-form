88eda284f3a510f8372c46f84568068f7f9d976f
Seth Zwerling <sazwerling@gmail.com>
2021-12-26 09:44:01 -0600

    temp nav/header

--- src/App.js
+++ src/App.js
@@ -1,83 +1,85 @@
-import React from 'react';
+import React from "react";
 
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  Link
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import InputComponent from './InputComponent';
-import Cards from './Cards';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { Container } from "reactstrap";
+import InputComponent from "./InputComponent";
+import Cards from "./Cards";
+import Nav from "./NavComponent";
+import unsplash from "./api/unsplash";
+import Image from "./ImageComponent";
 
-import unsplash from './api/unsplash';
-import Image from './ImageComponent';
+class App extends React.Component {
+   constructor(props) {
+      super(props);
+      this.state = {
+         frontValue: "",
+         backValue: "",
+         Id: 0,
+         arr: [],
+         image: [],
+      };
+   }
 
+   onSearchSubmit = async (term = "desk") => {
+      const response = await unsplash.get("/search/photos", {
+         params: { query: term },
+      });
+      this.setState({ image: response.data.results.slice(0, 3) });
+   };
 
+   componentDidMount = () => {
+      this.onSearchSubmit("desk");
+   };
 
-class App extends React.Component{
-      constructor(props){
-        super(props);
-        this.state = {
-          frontValue: '',
-          backValue: '',
-          Id: 0,
-          arr: [],
-          image: []
-        };
-      }
+   handleChangeFrontValue = (e) => {
+      this.setState({ frontValue: e.target.value });
+   };
 
+   handleChangeBackValue = (e) => {
+      this.setState({ backValue: e.target.value });
+   };
 
-      onSearchSubmit = async (term = "desk") => {
-        const response = await unsplash.get('/search/photos', {
-            params: { query: term },  
-        })
-        this.setState({ image: response.data.results.slice(0, 3) });
-    };
+   onAddItem = (e) => {
+      e.preventDefault();
 
-    componentDidMount = () => {
-      this.onSearchSubmit('desk')
-  }
+      this.setState({
+         arr: [
+            ...this.state.arr,
+            {
+               Id: this.state.Id,
+               front: this.state.frontValue,
+               back: this.state.backValue,
+            },
+         ],
+      });
+      this.onSearchSubmit(this.state.frontValue);
+      this.setState({ frontValue: "", backValue: "" });
+      this.setState({ Id: this.state.Id + 1 });
+   };
 
-  
-      handleChangeFrontValue = (e) => {
-        this.setState({ frontValue: e.target.value });
-      }
-
-      handleChangeBackValue = (e) => {
-        this.setState({ backValue: e.target.value });
-      }
-
-      onAddItem = (e) => {
-        e.preventDefault();
-        
-        this.setState({arr: [...this.state.arr, 
-          {
-            Id: this.state.Id,
-            front: this.state.frontValue, 
-            back: this.state.backValue
-          }
-        ]})
-        this.onSearchSubmit(this.state.frontValue);
-        this.setState({ frontValue: '', backValue: ''});
-        this.setState({ Id: this.state.Id + 1})
-                
-      }
-
-      render(){ 
-        return(
-          <BrowserRouter>
-          <Routes>
-            <Route path="/input" element={<InputComponent />} />
-            <Route path="cards" element={<Cards cardsArr={this.state.arr} />} />
-            <Route path="image" element={<Image images={this.state.image}/>} />
-          </Routes>
-        </BrowserRouter>
-         
-        )
-      }
-} 
+   render() {
+      return (
+         <Container>
+            <Nav />
+            <BrowserRouter>
+               <Routes>
+                  <Route path="/input" element={<InputComponent />} />
+                  <Route
+                     path="cards"
+                     element={<Cards cardsArr={this.state.arr} />}
+                  />
+                  <Route
+                     path="image"
+                     element={<Image images={this.state.image} />}
+                  />
+               </Routes>
+            </BrowserRouter>
+         </Container>
+      );
+   }
+}
 
 export default App;
---
+++ src/NavComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Nav = () => {
+    return(
+        <div>
+            I am the Nav/Header
+        </div>
+    )
+}
+
+export default Nav;
