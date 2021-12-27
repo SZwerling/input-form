511bb846ea040058db6337a5fdda799a0f838f82
Seth Zwerling <sazwerling@gmail.com>
2021-12-26 18:01:24 -0600

    toggler w hooks

--- src/NavComponent.js
+++ src/NavComponent.js
@@ -1,46 +1,52 @@
-import React from 'react';
-import { Navbar, Nav, Collapse, NavItem, NavLink, NavbarToggler, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem, NavbarText  } from 'reactstrap'
+import React, { useState } from "react";
+import {
+   Navbar,
+   Nav,
+   Collapse,
+   NavItem,
+   NavLink,
+   NavbarToggler,
+   UncontrolledDropdown,
+   DropdownToggle,
+   DropdownMenu,
+   DropdownItem,
+   NavbarText,
+} from "reactstrap";
 import { Link } from "react-router-dom";
 
 const Header = () => {
-    return(
-        <div>
-  <Navbar
-    fluid
-    color="light"
-    expand="md"
-    light
-  >
-    <NavbarToggler onClick={function noRefCheck(){}} />
-    <Collapse navbar>
-      <Nav
-        className="me-auto"
-        navbar
-      >
-          <NavItem>
-                <Link to="image">
-                    <NavLink>IMAGES</NavLink>
-                </Link>
-          </NavItem>
-          <NavItem>
-                <Link to="cards">
-                    <NavLink>CARDS</NavLink>
-                </Link>
-          </NavItem>
-          <NavItem>
-                <Link to="input">
-                    <NavLink>INPUT</NavLink>
-                </Link>
-          </NavItem>
-        
-      </Nav>
-      <NavbarText>
-        FLASHCARDS
-      </NavbarText>
-    </Collapse>
-  </Navbar>
-</div>
-    )
-}
+   const [navToggle, setNavToggle] = useState(true);
+
+   return (
+      <div>
+         <Navbar fluid color="light" expand="md" light>
+            <NavbarToggler
+               className="me-2"
+               onClick={() => setNavToggle(!navToggle)}
+            />
+            <Collapse isOpen={navToggle} navbar>
+               <Nav className="me-auto" navbar>
+                  <NavItem>
+                     <Link to="cards">
+                        <NavLink>CARDS</NavLink>
+                     </Link>
+                  </NavItem>
+                  <NavItem>
+                     <Link to="input">
+                        <NavLink>INPUT</NavLink>
+                     </Link>
+                  </NavItem>
+                  <NavItem>
+                     <Link to="image">
+                        <NavLink>IMAGES</NavLink>
+                     </Link>
+                  </NavItem>
+               </Nav>
+               <NavbarText>FLASHCARDS</NavbarText>
+            </Collapse>
+         </Navbar>
+      </div>
+   );
+};
 
 export default Header;
